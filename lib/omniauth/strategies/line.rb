0d53a31ac914d99050d8050054d91aee191fe62b
Vincent Li <vincent10e@gmail.com>
2020-08-28 23:48:08 +0800

    Add workaround

--- lib/omniauth/strategies/line.rb
+++ lib/omniauth/strategies/line.rb
@@ -23,8 +23,8 @@ module OmniAuth
 
       info do
         {
-          name:        raw_info['displayName'],
-          image:       raw_info['pictureUrl'],
+          name: raw_info['displayName'],
+          image: raw_info['pictureUrl'],
           description: raw_info['statusMessage']
         }
       end
@@ -36,6 +36,9 @@ module OmniAuth
         raise ::Timeout::Error
       end
 
+      def callback_url
+        full_host + script_name + callback_path
+      end
     end
   end
 end
